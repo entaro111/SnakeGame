ce6dc6b6bc5766885b660aab8dc4404e54ae5caf
brydzio234 <brydzio234@gmail.com>
2020-01-02 10:20:30 +0100

    drawing 3 starting rectangles of snake

--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SnakeGame
 {
     public partial class Form1 : Form
     {
+        Graphics graphics;
+        Snake snake = new Snake();
         public Form1()
         {
+            
+
             InitializeComponent();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            graphics = e.Graphics;
+            snake.drawSnake(graphics);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
         }
     }
 }
---
+++ Snake.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    class Snake
+    {
+
+        public Rectangle[] snakeParts;
+        private SolidBrush brush;
+        private int x, y, width, height;
+
+        public Snake()
+        {
+            snakeParts = new Rectangle[3];
+            brush = new SolidBrush(Color.DarkViolet);
+
+            x = 100;
+            y = 0;
+            width = 20;
+            height = 20;
+
+            for (int i = 0; i < snakeParts.Length; i++)
+            {
+                snakeParts[i] = new Rectangle(x, y, width, height);
+                x -= 20;
+            }
+
+        }
+
+        public void drawSnake(Graphics graphics)
+        {
+            foreach(Rectangle rectangle in snakeParts)
+            {
+                graphics.FillRectangle(brush, rectangle);
+            }
+        }
+
+
+    }
+}
